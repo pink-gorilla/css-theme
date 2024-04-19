6b884ccfe07cd9f3ebe922e26b620a30134d3df6
awb99 <awb99@gmail.com>
2024-04-19 16:13:36 -0500

    cljs-service added

--- src/frontend/css.clj
+++ src/frontend/css.clj
@@ -7,3 +7,11 @@
                     (map :theme))]
     {:available (reduce merge {} (map :available themes))
      :current (reduce merge {} (map :current themes))}))
+
+
+(defn config-theme [_module-name _config exts _default-config]
+  ;(let [module-name (if (string? module-name)
+  ;                    (keyword module-name)
+  ;                    module-name)]
+  ;  (or (get config module-name) default-config)) 
+  (get-theme-config exts))
---
+++ src/frontend/css.cljs
@@ -0,0 +1,12 @@
+(ns frontend.css
+  (:require
+   [re-frame.core :refer [dispatch]]
+   [frontend.css.events]
+   [frontend.css.loading]
+   [frontend.css.subscriptions]))
+
+(defn start-theme [theme]
+  (dispatch [:css/init theme]))
+
+
+
